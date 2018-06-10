28594588a9f59a51606150e64934020a8247705d
Hal Finkel <hfinkel@anl.gov>
2018-06-10 11:38:48 -0500

    make the js return something interesting

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -38,7 +38,8 @@ int main(int argc, char *argv[]) {
   while (!cv.wait_for(ul, 2000ms, [&] { return done; })) {
     auto r = w.run_script("var node = document.createElement('li');"
                           "node.appendChild(document.createTextNode('Time has passed'));"
-                          "document.getElementById('dl').appendChild(node); 1;");
+                          "document.getElementById('dl').appendChild(node); "
+                          "var d = new Date(); d.getTime();");
     std::cout << "got from script: " << r.get() << "\n";
   }
 
