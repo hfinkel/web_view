c004be7d3d016b12abb8025e1ee6fec28e14f018
Hal Finkel <hfinkel@anl.gov>
2018-06-10 08:31:01 -0500

    make everything build

--- web_view.h
+++ web_view.h
@@ -24,21 +24,21 @@ namespace wv {
 #endif
 
   namespace web_view_detail {
+    using HandlerFunc = std::function<bool(const std::string &, std::ostream &)>;
+
     struct impl {
       virtual ~impl() { }
 
       virtual std::future<std::error_code> display_from_uri(const std::string &uri) = 0;
-
-      using HandlerFunc = std::function<bool(const std::string &, std::ostream &)>;
       virtual void register_uri_scheme_handler(const std::string &scheme,
                                                HandlerFunc *func) = 0;
     };
 
-    impl *make_web_view_detail(const std::string &title);
+    impl *make_web_view_impl(const std::string &title);
   }
 
   struct web_view {
-    web_view(const std::string &title = "") : m(make_web_view_detail(title)) { }
+    web_view(const std::string &title = "") : m(web_view_detail::make_web_view_impl(title)) { }
 
     std::future<std::error_code> display_from_uri(const std::string &uri) {
       return m->display_from_uri(uri);
@@ -46,7 +46,7 @@ namespace wv {
 
     template <typename URISchemeHandler>
     void set_uri_scheme_handler(const std::string &scheme, URISchemeHandler handler) {
-      m->register_uri_scheme_handler(scheme, new HandlerFunc(handler));
+      m->register_uri_scheme_handler(scheme, new web_view_detail::HandlerFunc(handler));
     }
 
   private:
