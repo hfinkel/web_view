dd9c6cfcdcf3235e2d3a3e1674b9afca394a3fbd
Hal Finkel <hfinkel@anl.gov>
2018-06-10 07:44:54 -0500

    write more code

--- web_view.h
+++ web_view.h
@@ -27,13 +27,11 @@ namespace wv {
     struct impl {
       virtual ~impl() { }
 
-      virtual std::future<std::error_code> display_html_from_uri(const std::string &uri) = 0;
-      virtual std::future<std::error_code> display_html(std::string_view html,
-                                                        const std::string &effective_uri) = 0;
+      virtual std::future<std::error_code> display_from_uri(const std::string &uri) = 0;
 
       using HandlerFunc = std::function<bool(const std::string &, std::ostream &)>;
-      virtual bool register_uri_scheme_handler(const std::string &scheme,
-                                               const HandlerFunc &func) = 0;
+      virtual void register_uri_scheme_handler(const std::string &scheme,
+                                               HandlerFunc *func) = 0;
     };
 
     impl *make_web_view_detail(const std::string &title);
@@ -42,18 +40,13 @@ namespace wv {
   struct web_view {
     web_view(const std::string &title = "") : m(make_web_view_detail(title)) { }
 
-    std::future<std::error_code> display_html_from_uri(const std::string &uri) {
-      return m->display_html_from_uri(uri);
-    }
-
-    std::future<std::error_code> display_html(std::string_view html,
-                                              const std::string &effective_uri) {
-      return m->display_html(html, effective_uri);
+    std::future<std::error_code> display_from_uri(const std::string &uri) {
+      return m->display_from_uri(uri);
     }
 
     template <typename URISchemeHandler>
-    bool set_uri_scheme_handler(const std::string &scheme, URISchemeHandler handler) {
-      return m->register_uri_scheme_handler(scheme, handler);
+    void set_uri_scheme_handler(const std::string &scheme, URISchemeHandler handler) {
+      m->register_uri_scheme_handler(scheme, new HandlerFunc(handler));
     }
 
   private:
